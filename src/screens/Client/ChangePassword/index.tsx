9a677d0e27ee7b5e02d57fc5c9fdae747efd1d43
Carlos <carlos.soares@clicksoft.com.br>
2024-07-15 16:03:12 -0300

    fix: prettier in project

--- src/screens/Client/ChangePassword/index.tsx
+++ src/screens/Client/ChangePassword/index.tsx
@@ -1,96 +1,111 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useNavigation } from "@react-navigation/native";
+import React, {useState} from 'react'
+import {useForm} from 'react-hook-form'
+import {useNavigation} from '@react-navigation/native'
 
-import { Button } from "@components/Button";
-import { Header } from "@components/Header";
+import {Button} from '@components/Button'
+import {Header} from '@components/Header'
 
-import {
-  BoxButtons,
-  Container,
-  InputsContainer,
-  TextError
-} from "./styles";
+import {BoxButtons, Container, InputsContainer, TextError} from './styles'
 
-import { InputUnMasked } from "@components/InputUnMasked";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { ChangePasswordSchema } from "../../../schemas/changePassword";
+import {InputUnMasked} from '@components/InputUnMasked'
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
+import {yupResolver} from '@hookform/resolvers/yup'
+import {ChangePasswordSchema} from '../../../schemas/changePassword'
 
 type FormData = {
-  currentPassword: string;
-  newPassword: string;
-  confirmationPassword: string;
-};
+    currentPassword: string
+    newPassword: string
+    confirmationPassword: string
+}
 
 export function ChangePassword() {
-  const navigation = useNavigation()
-  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
-    resolver: yupResolver(ChangePasswordSchema)
-  });
+    const navigation = useNavigation()
+    const {
+        control,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<FormData>({
+        resolver: yupResolver(ChangePasswordSchema)
+    })
 
-  const [showPassword, setShowPassword] = useState(true)
-  const [showCurrentPassword, setCurrentShowPassword] = useState(true)
-  const [showConfirmedPassword, setShowConfirmedPassword] = useState(true)
+    const [showPassword, setShowPassword] = useState(true)
+    const [showCurrentPassword, setCurrentShowPassword] = useState(true)
+    const [showConfirmedPassword, setShowConfirmedPassword] = useState(true)
 
-  const onSubmit = (data: FormData) => {
-    console.log(data)
-    navigation.navigate('PasswordResetSuccess')
-  }
-  return (
-    <Container>
-      <KeyboardAwareScrollView>
-        <Header title="Alterar senha" activeButtonGoBack={true} />
+    const onSubmit = (data: FormData) => {
+        console.log(data)
+        navigation.navigate('PasswordResetSuccess')
+    }
+    return (
+        <Container>
+            <KeyboardAwareScrollView>
+                <Header title="Alterar senha" activeButtonGoBack={true} />
 
-        <InputsContainer>
-          <InputUnMasked
-            control={control}
-            label='Senha atual'
-            name='currentPassword'
-            eye={true}
-            showPassword={showPassword}
-            setShowPassword={setShowPassword}
-            secureTextEntry={showPassword}
-            placeholder='Digite sua senha'
-            error={
-              errors.newPassword && <TextError>{errors.newPassword.message}</TextError>
-            }
-          />
+                <InputsContainer>
+                    <InputUnMasked
+                        control={control}
+                        label="Senha atual"
+                        name="currentPassword"
+                        eye={true}
+                        showPassword={showPassword}
+                        setShowPassword={setShowPassword}
+                        secureTextEntry={showPassword}
+                        placeholder="Digite sua senha"
+                        error={
+                            errors.newPassword && (
+                                <TextError>
+                                    {errors.newPassword.message}
+                                </TextError>
+                            )
+                        }
+                    />
 
-          <InputUnMasked
-            control={control}
-            label='Nova senha'
-            name='newPassword'
-            eye={true}
-            showPassword={showCurrentPassword}
-            setShowPassword={setCurrentShowPassword}
-            secureTextEntry={showCurrentPassword}
-            placeholder='Digite sua senha'
-            error={
-              errors.newPassword && <TextError>{errors.newPassword.message}</TextError>
-            }
-          />
+                    <InputUnMasked
+                        control={control}
+                        label="Nova senha"
+                        name="newPassword"
+                        eye={true}
+                        showPassword={showCurrentPassword}
+                        setShowPassword={setCurrentShowPassword}
+                        secureTextEntry={showCurrentPassword}
+                        placeholder="Digite sua senha"
+                        error={
+                            errors.newPassword && (
+                                <TextError>
+                                    {errors.newPassword.message}
+                                </TextError>
+                            )
+                        }
+                    />
 
-          <InputUnMasked
-            control={control}
-            label='Repetir nova senha'
-            name='confirmationPassword'
-            eye={true}
-            secureTextEntry={showConfirmedPassword}
-            showPassword={showConfirmedPassword}
-            setShowPassword={setShowConfirmedPassword}
-            placeholder='Confirme sua senha'
-            error={
-              errors.confirmationPassword && <TextError>{errors.confirmationPassword.message}</TextError>
-            }
-          />
-        </InputsContainer>
+                    <InputUnMasked
+                        control={control}
+                        label="Repetir nova senha"
+                        name="confirmationPassword"
+                        eye={true}
+                        secureTextEntry={showConfirmedPassword}
+                        showPassword={showConfirmedPassword}
+                        setShowPassword={setShowConfirmedPassword}
+                        placeholder="Confirme sua senha"
+                        error={
+                            errors.confirmationPassword && (
+                                <TextError>
+                                    {errors.confirmationPassword.message}
+                                </TextError>
+                            )
+                        }
+                    />
+                </InputsContainer>
 
-        <BoxButtons>
-          <Button title="Salvar" onPress={handleSubmit(onSubmit)} />
-          <Button title="Cancelar" variant="secondary" onPress={() => navigation.goBack()} />
-        </BoxButtons>
-      </KeyboardAwareScrollView>
-    </Container>
-  )
+                <BoxButtons>
+                    <Button title="Salvar" onPress={handleSubmit(onSubmit)} />
+                    <Button
+                        title="Cancelar"
+                        variant="secondary"
+                        onPress={() => navigation.goBack()}
+                    />
+                </BoxButtons>
+            </KeyboardAwareScrollView>
+        </Container>
+    )
 }
--- src/screens/Client/Support/index.tsx
+++ src/screens/Client/Support/index.tsx
@@ -1,51 +1,56 @@
-import React from "react";
+import React from 'react'
 
-import * as S from "./styles";
-import { Navbar } from "@components/Navbar";
-import { InputUnMasked } from "@components/InputUnMasked";
-import { useForm } from "react-hook-form";
-import { Button } from "@components/Button";
-import { Text, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import * as S from './styles'
+import {Navbar} from '@components/Navbar'
+import {InputUnMasked} from '@components/InputUnMasked'
+import {useForm} from 'react-hook-form'
+import {Button} from '@components/Button'
+import {Text, View} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
 
-export function Support (){
-  const navigation = useNavigation()
+export function Support() {
+    const navigation = useNavigation()
 
-  const {control} = useForm()
+    const {control} = useForm()
 
-  return <S.Container>
-          <Navbar />
+    return (
+        <S.Container>
+            <Navbar />
 
-          <S.ContainerBody
-          contentContainerStyle={{flexGrow:1}}
-          showsVerticalScrollIndicator={false}
-          >
-          <S.Title>SUPORTE</S.Title>
+            <S.ContainerBody
+                contentContainerStyle={{flexGrow: 1}}
+                showsVerticalScrollIndicator={false}>
+                <S.Title>SUPORTE</S.Title>
 
-          <Text>
-          Descreva seu problema com o máximo possível de detalhes
-          para que nossa equipe possa ajudar.
-          </Text>
+                <Text>
+                    Descreva seu problema com o máximo possível de detalhes para
+                    que nossa equipe possa ajudar.
+                </Text>
 
-          <InputUnMasked
-            control={control}
-            label='Componente'
-            name=''
-            placeholder='Qual componente está com problemas?'
-            />
-          
-          <InputUnMasked
-            control={control}
-            label='Descrição do problema'
-            name=''
-            placeholder='Descreva o que está acontecendo.'
-            />
+                <InputUnMasked
+                    control={control}
+                    label="Componente"
+                    name=""
+                    placeholder="Qual componente está com problemas?"
+                />
 
-            <View style={{flex:1}} />
+                <InputUnMasked
+                    control={control}
+                    label="Descrição do problema"
+                    name=""
+                    placeholder="Descreva o que está acontecendo."
+                />
 
-            <Button title="Enviar" onPress={() => {}} />
-            <Button title="DÚVIDAS FREQUENTES" onPress={() => {}} variant="secondary"/>
-            <Button title="Voltar" onPress={() =>navigation.goBack()} />
-          </S.ContainerBody>
+                <View style={{flex: 1}} />
+
+                <Button title="Enviar" onPress={() => {}} />
+                <Button
+                    title="DÚVIDAS FREQUENTES"
+                    onPress={() => {}}
+                    variant="secondary"
+                />
+                <Button title="Voltar" onPress={() => navigation.goBack()} />
+            </S.ContainerBody>
         </S.Container>
+    )
 }
