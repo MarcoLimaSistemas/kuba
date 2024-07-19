eedfe7243460102782bd9b4ec5b283d2732d1b8a
DouglasFeliphe <douglasfeliphe@hotmail.com>
2024-07-19 00:45:56 -0300

    config: fix absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,33 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      ['react-native-reanimated/plugin'],
-      ['module-resolver',
-        {
-          root: ['./scr'],
-          extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.android.js',
-            '.android.tsx',
-            '.ios.js',
-            '.ios.tsx',
-          ],
-          alias: {
-            '@assets': './src/assets',
-            '@components': './src/components',
-            '@hooks': './src/hooks',
-            '@screens': './src/screens',
-            '@schemas': './src/schemas',
-            '@models': './src/models',
-          },
-        },
-      ],
-    ],
-  };
+	api.cache(true);
+	return {
+		presets: ['module:metro-react-native-babel-preset'],
+		plugins: [
+			['react-native-reanimated/plugin'],
+			[
+				'module-resolver',
+				{
+					extensions: [
+						'.js',
+						'.jsx',
+						'.ts',
+						'.tsx',
+						'.android.js',
+						'.android.tsx',
+						'.ios.js',
+						'.ios.tsx',
+						'.json'
+					],
+					alias: {
+						'@assets': './src/assets',
+						'@components': './src/components',
+						'@hooks': './src/hooks',
+						'@screens': './src/screens',
+						'@schemas': './src/schemas',
+						'@models': './src/models'
+					}
+				}
+			]
+		]
+	};
 };
