3ce594761c51ecc14060e89ae12f1e745585d891
Carlos <carlos.soares@clicksoft.com.br>
2024-07-19 11:02:38 -0300

    fix: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,8 +16,8 @@ export const Routes = () => {
         <>
             <NavigationContainer ref={navigationRef}>
                 {/* <AuthRoutes /> */}
-                <AppRoutes />
-                {/* {user.id !== undefined ? <AppRoutes /> : <AuthRoutes />} */}
+                {/* <AppRoutes /> */}
+                {user.id !== undefined ? <AppRoutes /> : <AuthRoutes />}
             </NavigationContainer>
             <Toast />
         </>
