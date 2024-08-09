f37efe993de72d6e3e669ddf9c63989edbdacc5b
Carlos <carlos.soares@clicksoft.com.br>
2024-08-09 16:18:57 -0300

    fix: space in card school kuba

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,88 +1,92 @@
-import React, { useRef } from 'react';
-import { CardDevice } from '@components/CardDevice';
-import { ActivityIndicator, FlatList, View } from 'react-native';
-import { Spacer } from '@components/Spacer';
-import { Icons } from '@assets/icons';
+import React, {useRef} from 'react';
+import {CardDevice} from '@components/CardDevice';
+import {ActivityIndicator, FlatList, View} from 'react-native';
+import {Spacer} from '@components/Spacer';
+import {Icons} from '@assets/icons';
 import Text from '@components/Text';
 
 import * as S from './styles';
-import { scale } from 'react-native-size-matters';
+import {scale} from 'react-native-size-matters';
 
-import { Modalize } from 'react-native-modalize';
-import { ModalDevices } from '@components/ModalDevices';
-import { Device } from '@models/device';
+import {Modalize} from 'react-native-modalize';
+import {ModalDevices} from '@components/ModalDevices';
+import {Device} from '@models/device';
 import theme from '../../styles/theme';
 
 interface ICarousselProps {
-	devices: Device[];
-	isLoading: boolean;
+  devices: Device[];
+  isLoading: boolean;
 }
 
-export function Carousel({ devices, isLoading }: ICarousselProps) {
-	const modalizeRef = useRef<Modalize>(null);
+export function Carousel({devices, isLoading}: ICarousselProps) {
+  const modalizeRef = useRef<Modalize>(null);
 
-	const onOpen = () => modalizeRef.current?.open();
-	const onClose = () => modalizeRef.current?.close();
+  const onOpen = () => modalizeRef.current?.open();
+  const onClose = () => modalizeRef.current?.close();
 
-	return (
-		<>
-			<Text
-				color="#656565"
-				variant="bold"
-				fontSize={18}
-				style={{ marginLeft: scale(16) }}>
-				Meus Dispositivos
-			</Text>
+  return (
+    <>
+      <Text
+        color="#656565"
+        variant="bold"
+        fontSize={18}
+        style={{marginLeft: scale(16)}}>
+        Meus Dispositivos
+      </Text>
 
-			<Spacer h={16} />
+      <Spacer h={16} />
 
-			<FlatList
-				data={devices}
-				showsHorizontalScrollIndicator={false}
-				contentContainerStyle={{
-					paddingLeft: scale(16)
-				}}
-				horizontal
-				renderItem={({ item }) => (
-					<CardDevice
-						key={item.id}
-						id={item.id}
-						imgURL={item.img_url}
-						title={item.name}
-						isBluetooth={JSON.parse(item.is_bluetooth)}
-					/>
-				)}
-				ListFooterComponent={
-					<S.ButtonAdd
-						onPress={onOpen}
-						style={{
-							elevation: 8,
-							marginVertical: scale(4),
-							marginRight: scale(8)
-						}}>
-						<Icons.Plus color={'#6E6E6E'} />
+      <View>
+        <FlatList
+          data={devices}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingLeft: scale(16),
+            borderWidth: 2,
+            borderColor: 'red',
+          }}
+          horizontal
+          renderItem={({item}) => (
+            <CardDevice
+              key={item.id}
+              id={item.id}
+              imgURL={item.img_url}
+              title={item.name}
+              isBluetooth={JSON.parse(item.is_bluetooth)}
+            />
+          )}
+          ListFooterComponent={
+            <S.ButtonAdd
+              onPress={onOpen}
+              style={{
+                elevation: 4,
+                marginVertical: scale(4),
+                marginRight: scale(8),
+              }}>
+              <Icons.Plus color={'#6E6E6E'} />
 
-						<Text style={{ textAlign: 'center' }} fontSize={14}>
-							Adicionar novo dispositivo
-						</Text>
-					</S.ButtonAdd>
-				}
-				ListEmptyComponent={() =>
-					isLoading ? (
-						<View
-							style={{
-								marginRight: scale(40),
-								marginLeft: scale(16),
-								justifyContent: 'center',
-								alignItems: 'center'
-							}}>
-							<ActivityIndicator color={theme.COLORS.gold_100} />
-						</View>
-					) : null
-				}
-			/>
+              <Text style={{textAlign: 'center'}} fontSize={14}>
+                Adicionar novo dispositivo
+              </Text>
+            </S.ButtonAdd>
+          }
+          ListEmptyComponent={() =>
+            isLoading ? (
+              <View
+                style={{
+                  marginRight: scale(40),
+                  marginLeft: scale(16),
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <ActivityIndicator color={theme.COLORS.gold_100} />
+              </View>
+            ) : null
+          }
+        />
+      </View>
 
-			<ModalDevices ref={modalizeRef} close={onClose} />
-		</>
-	);
+      <ModalDevices ref={modalizeRef} close={onClose} />
+    </>
+  );
 }
--- src/screens/Client/Home/index.tsx
+++ src/screens/Client/Home/index.tsx
@@ -1,104 +1,100 @@
 import React from 'react';
 
-import { Carousel } from '@components/Carousel';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
+import {Carousel} from '@components/Carousel';
+import {useNavigation} from '@react-navigation/native';
 
-import { background, backgroundSecondary } from '@assets/images';
+import {background, backgroundSecondary} from '@assets/images';
 
 import {
-	Container,
-	ContainerHeader,
-	ContainerSchoolKuba,
-	ImageHeaderHome,
-	ImageSchoolKuba
+  Container,
+  ContainerHeader,
+  ContainerSchoolKuba,
+  ImageHeaderHome,
+  ImageSchoolKuba,
 } from './styles';
 
-import { Header } from '@components/Header';
+import {Header} from '@components/Header';
 import Text from '@components/Text';
-import { scale } from 'react-native-size-matters';
-import { Spacer } from '@components/Spacer';
-import { View } from 'react-native';
-import { useQuery } from '@tanstack/react-query';
-import { getProducts } from '@services/product';
-import { useAuth } from '@hooks/auth';
+import {scale} from 'react-native-size-matters';
+import {Spacer} from '@components/Spacer';
+import {View} from 'react-native';
+import {useQuery} from '@tanstack/react-query';
+import {getProducts} from '@services/product';
+import {useAuth} from '@hooks/auth';
 
 export function Home() {
-	const { user } = useAuth();
+  const {user} = useAuth();
 
-	const { data: devices, isLoading } = useQuery({
-		queryKey: ['Devices'],
-		queryFn: () => getProducts(user?.id)
-	});
+  const {data: devices, isLoading} = useQuery({
+    queryKey: ['Devices'],
+    queryFn: () => getProducts(user?.id),
+  });
 
-	const navigation = useNavigation();
+  const navigation = useNavigation();
 
-	return (
-		<>
-			<ContainerHeader>
-				<Header typeLogo="white" />
-				<ImageHeaderHome source={background} />
+  return (
+    <>
+      <ContainerHeader>
+        <Header typeLogo="white" />
+        <ImageHeaderHome source={background} />
 
-				<View
-					style={{
-						paddingHorizontal: scale(16),
-						marginTop: 'auto'
-					}}>
-					<Text color="#FFF" variant="light">
-						Escola Kuba
-					</Text>
-					<Spacer h={4} />
-					<Text variant="bold" color="#FFF">
-						Como tirar o melhor som de um fone?
-					</Text>
-				</View>
-				<Spacer h={16} />
-			</ContainerHeader>
+        <View
+          style={{
+            paddingHorizontal: scale(16),
+            marginTop: 'auto',
+          }}>
+          <Text color="#FFF" variant="light">
+            Escola Kuba
+          </Text>
+          <Spacer h={4} />
+          <Text variant="bold" color="#FFF">
+            Como tirar o melhor som de um fone?
+          </Text>
+        </View>
+        <Spacer h={16} />
+      </ContainerHeader>
 
-			<Spacer h={32} />
+      <Spacer h={32} />
 
-			<Carousel devices={devices} isLoading={isLoading} />
+      <Carousel devices={devices} isLoading={isLoading} />
 
-			<Container>
-				<ContainerSchoolKuba
-					onPress={() => navigation.navigate('School')}>
-					<ImageSchoolKuba source={backgroundSecondary} />
+      <Container>
+        <Spacer h={16} />
+        <ContainerSchoolKuba onPress={() => navigation.navigate('School')}>
+          <ImageSchoolKuba source={backgroundSecondary} />
 
-					<View
-						style={{
-							position: 'absolute',
-							justifyContent: 'center',
-							alignItems: 'center',
-							top: 0,
-							left: 0,
-							bottom: 0,
-							right: 0,
-							flex: 1,
-							paddingHorizontal: scale(16)
-						}}>
-						<View>
-							<Text
-								color="#FFF"
-								variant="bold"
-								style={{
-									textAlign: 'center',
-									letterSpacing: scale(6)
-								}}>
-								ESCOLA KUBA
-							</Text>
-							<Spacer h={8} />
-							<Text
-								fontSize={12}
-								color="#FFF"
-								style={{ textAlign: 'center' }}>
-								Aprenda mais sobre o mundo do áudio
-							</Text>
-						</View>
-					</View>
-				</ContainerSchoolKuba>
+          <View
+            style={{
+              position: 'absolute',
+              justifyContent: 'center',
+              alignItems: 'center',
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              flex: 1,
+              paddingHorizontal: scale(16),
+            }}>
+            <View>
+              <Text
+                color="#FFF"
+                variant="bold"
+                style={{
+                  textAlign: 'center',
+                  letterSpacing: scale(6),
+                }}>
+                ESCOLA KUBA
+              </Text>
+              <Spacer h={8} />
+              <Text fontSize={12} color="#FFF" style={{textAlign: 'center'}}>
+                Aprenda mais sobre o mundo do áudio
+              </Text>
+            </View>
+          </View>
+        </ContainerSchoolKuba>
 
-				<Spacer h={16} />
-			</Container>
-		</>
-	);
+        <Spacer h={16} />
+      </Container>
+    </>
+  );
 }
