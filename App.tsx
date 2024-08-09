6dec7d1b60e90ae5b95df8cbd6bb1dcc66e0be25
Carlos <carlos.soares@clicksoft.com.br>
2024-08-09 15:23:38 -0300

    fix: school screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import {AppProvider} from './src/hooks';
 import {Routes} from './src/routes';
 import {StatusBar} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
 
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
-import {DevToolsBubble} from 'react-native-react-query-devtools';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 export const queryClient = new QueryClient();
 
 const App = () => {
-  useEffect(() => {
-    SplashScreen.hide(); //hides the splash screen on app load.
-  }, []);
-
   return (
     <>
       <QueryClientProvider client={queryClient}>
