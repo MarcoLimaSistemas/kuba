c172bf57dbb433de6465c5723f431affd4cb1714
wesleywcr <wesley.rodrigues@clicksoft.com.br>
2024-08-30 15:27:40 -0300

    refactor: update styles in component search

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -2,18 +2,16 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-	/* border: 1px solid red; */
-	display: flex;
 	flex-direction: row;
 	justify-content: center;
 	padding: 0 ${RFValue(16)}px;
-	margin-top: ${RFValue(16)}px;
+	margin-top: ${RFValue(46)}px;
 	margin-bottom: ${RFValue(16)}px;
 `;
 
 export const ContainerInput = styled.TextInput`
 	flex: 1;
-	height: ${RFValue(56)}px;
+	height: ${RFValue(46)}px;
 	border: 2px solid ${({ theme }) => theme.COLORS.white_100};
 	border-radius: 8px;
 	color: ${({ theme }) => theme.COLORS.white_100};
@@ -24,7 +22,7 @@ export const ContainerInput = styled.TextInput`
 export const ButtonSearch = styled.TouchableOpacity`
 	justify-content: center;
 	align-items: center;
-	width: ${RFValue(56)}px;
-	height: ${RFValue(56)}px;
+	width: ${RFValue(46)}px;
+	height: ${RFValue(46)}px;
 	border-radius: 8px;
 `;
