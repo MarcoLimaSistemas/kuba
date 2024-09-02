5fc7994f2b2d71f0d46bf47aecb6071a9f489208
wesleywcr <wesley.rodrigues@clicksoft.com.br>
2024-09-02 17:51:31 -0300

    feat: add new types in preset

--- src/models/preset.ts
+++ src/models/preset.ts
@@ -5,3 +5,42 @@ export interface IPresets {
 	genreId: string;
 	isPublic: boolean;
 }
+export interface IPresetResponse {
+	meta: Meta;
+	data: IPreset[];
+}
+
+export interface  IPreset {
+	id:               number;
+	genre_id:         number;
+	name:             string;
+	description:      string;
+	preamp:           number;
+	img_url:          string;
+	is_public:        boolean;
+	client_id:        string|null;
+	created_at:       string;
+	updated_at:       string;
+	equalizerConfigs: IEqualizerConfig[];
+	settings?:        any;
+}
+
+export interface IEqualizerConfig {
+	id:               number;
+	frequency:        string;
+	decibel_quantity: number;
+	preset_id:        number;
+}
+
+export interface Meta {
+	total:             number;
+	per_page:          number;
+	current_page:      number;
+	last_page:         number;
+	first_page:        number;
+	first_page_url:    string;
+	last_page_url:     string;
+	next_page_url:     string|null;
+	previous_page_url: string|null;
+}
+
