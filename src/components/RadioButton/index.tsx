f11089f8218050cb5301201eb271a6ccdac326ae
wesleywcr <wesley.rodrigues@clicksoft.com.br>
2024-11-13 17:05:11 -0300

    refactor: :lipstick: update styles radio button

--- src/components/RadioButton/index.tsx
+++ src/components/RadioButton/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+import * as S from './styles';
+import Text from '@components/Text';
 
 type RadioButtonProps = {
   options: string[];
@@ -9,49 +11,18 @@ type RadioButtonProps = {
 
 const RadioButton: React.FC<RadioButtonProps> = ({ options, selectedOption, onSelect }) => {
   return (
-    <View>
+    <S.Container>
       {options.map((option) => (
-        <TouchableOpacity key={option} style={styles.radioButton} onPress={() => onSelect(option)}>
-          <View style={styles.radioCircle}>
-            {selectedOption === option && <View style={styles.innerCircle} />}
-          </View>
-          <Text style={styles.radioText}>{option}</Text>
-        </TouchableOpacity>
+        <S.Button key={option} isSelected={selectedOption === option}  onPress={() => onSelect(option)}>
+          <S.ContainerText>
+          <Text color={selectedOption === option?"white":'black'} fontSize={14}>Band</Text>
+          <Text color={selectedOption === option?"white":'black'} fontSize={14}>{option}</Text>
+          </S.ContainerText>
+        </S.Button>
       ))}
-    </View>
+    </S.Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  radioButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  radioCircle: {
-    height: 24,
-    width: 24,
-    borderRadius: 12,
-    borderWidth: 2,
-    borderColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  innerCircle: {
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    backgroundColor: '#000',
-  },
-  radioText: {
-    marginLeft: 10,
-    color: '#000',
-  },
-});
 
 export default RadioButton;
---
+++ src/components/RadioButton/styles.ts
@@ -0,0 +1,29 @@
+import { scale } from 'react-native-size-matters';
+import styled from 'styled-components/native';
+
+type ButtonProps = {
+  isSelected:boolean
+}
+export const Container = styled.View`
+flex: 1;
+flex-direction: row;
+justify-content: space-between;
+`;
+
+
+
+export const ContainerText = styled.View`
+align-items: center;
+justify-content: center;
+`;
+
+export const Button = styled.TouchableOpacity<ButtonProps>`
+
+background-color: ${({ isSelected,theme }) => isSelected?theme.COLORS.black:theme.COLORS.white_100};
+padding: ${scale(8)}px;
+flex-direction: row;
+align-items: center;
+border-radius: ${scale(16)}px;
+margin-top: ${scale(12)}px;
+margin-bottom: ${scale(12)}px;
+`;
