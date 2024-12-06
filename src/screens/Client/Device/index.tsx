2bc86aaf783847ff72c041611e83d9261eecb890
wesleywcr <wesley.rodrigues@clicksoft.com.br>
2024-12-06 16:24:11 -0300

    refactor: update screen device

--- src/screens/Client/Device/index.tsx
+++ src/screens/Client/Device/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 
-import { Button } from '@components/Button';
+
 import { CarouselProfile } from '@components/CarouselProfile';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import RNBluetoothClassic, { BluetoothDevice } from 'react-native-bluetooth-classic';
 
 import { Headset, Info, Lighting, Settings } from '@assets/icons';
@@ -27,7 +27,7 @@ import { useBluetooth } from '../../../context/BluetoothContext';
 import { Header } from '@components/Header';
 import Text from '@components/Text';
 import { scale } from 'react-native-size-matters';
-import {  ModalPreset } from '@components/ModalPreset';
+import { ModalPreset } from '@components/ModalPreset';
 import { Modalize } from 'react-native-modalize';
 import { useQuery } from '@tanstack/react-query';
 import { getPresets, getPresetsPublics } from '@services/preset';
@@ -39,24 +39,32 @@ import DeviceListScreen from '@components/Equalizer/deviceList';
 import { StateChangeEvent } from 'react-native-bluetooth-classic/lib/BluetoothEvent';
 import { HeaderEqualizer, IMyPresets } from '@components/Equalizer/Header';
 import { IPreset } from '@models/preset';
+import { getDataPresets } from '@services/internal-storage';
+
+
 
 
 
 export interface IFrequency {
 	frequency: string;
 	decibelQuantity: number;
-	quality:number
+	quality: number
 }
 
 interface AppState {
-  device?: BluetoothDevice;
-  bluetoothEnabled: boolean;
+	device?: BluetoothDevice;
+	bluetoothEnabled: boolean;
 }
 
 export function Device() {
-	const route = useRoute();
 
-	const device = route.params as any;
+	const device = {
+		id: 1,
+		imgURL: "https://kuba-staging-api-files.s3.sa-east-1.amazonaws.com/product/941-1",
+		isBluetooth: true,
+		name: "Disco 2 Clássico"
+	}
+
 
 	const [scrollEnabled, setScrollEnabled] = useState(true);
 	const [currentPreset, setCurrentPreset] = useState<IPreset | null>(null);
@@ -66,37 +74,27 @@ export function Device() {
 
 	const navigation = useNavigation<any>();
 
-	const { connectedDevice, connectToDevice,state,setState } = useBluetooth();
+	const { connectedDevice, connectToDevice, state, setState } = useBluetooth();
 
-	const { user } = useAuth();
-
-
-
-	const { data: personalitiesData, isFetched } = useQuery({
-		queryKey: ['PersonalitiesOnDeviceScreen'],
-		queryFn: () => getPresets(user?.id, undefined, 1, false, 5)
-	});
-
-	const personalities = useMemo(() => {
-		return personalitiesData?.data ?? [];
-	}, [personalitiesData]);
 
 	const { data: profilesData } = useQuery({
-		queryKey: ['PresetsPublicsOnDeviceScreen'],
-		queryFn: () => getPresetsPublics(user?.id, undefined, 1, 5),
-		enabled: isFetched
+		queryKey: ['MyPresets'],
+		queryFn: async () => await getDataPresets(),
+		//enabled: isFetched
 	});
 
+
 	const profiles = useMemo(() => {
-		return profilesData?.data ?? [];
+		return profilesData ?? [];
 	}, [profilesData]);
+
 
 	const handleScrollEnabled = (enabled: boolean) => {
 		setScrollEnabled(enabled);
 	};
 
 	const handlePreset = (preset: IMyPresets) => {
-		const form ={
+		const form = {
 			name: preset.label,
 			...preset
 		} as unknown as IPreset
@@ -115,86 +113,87 @@ export function Device() {
 	const closeModal = () => modalizeRef.current?.close();
 
 	// const [state, setState] = useState<AppState>({
-  //   device: undefined,
-  //   bluetoothEnabled: true,
-  // });
+	//   device: undefined,
+	//   bluetoothEnabled: true,
+	// });
 
-  let enabledSubscription: any;
-  let disabledSubscription: any;
+	let enabledSubscription: any;
+	let disabledSubscription: any;
 
-  const selectDevice = (device: BluetoothDevice) => {
-    console.log('App::selectDevice() called with: ', device);
-    setState((prevState) => ({ ...prevState, device }));
-  };
+	const selectDevice = (device: BluetoothDevice) => {
+		console.log('App::selectDevice() called with: ', device);
+		setState((prevState) => ({ ...prevState, device }));
+	};
 
-  const checkBluetoothEnabled = async () => {
-    try {
-      console.log('App::componentDidMount Checking bluetooth status');
-      const enabled = await RNBluetoothClassic.isBluetoothEnabled();
+	const checkBluetoothEnabled = async () => {
+		try {
+			console.log('App::componentDidMount Checking bluetooth status');
+			const enabled = await RNBluetoothClassic.isBluetoothEnabled();
 
-      console.log(`App::componentDidMount Status: ${enabled}`);
-      setState((prevState) => ({ ...prevState, bluetoothEnabled: enabled }));
-    } catch (error) {
-      console.log('App::componentDidMount Status Error: ', error);
-      setState((prevState) => ({ ...prevState, bluetoothEnabled: false }));
-    }
-  };
+			console.log(`App::componentDidMount Status: ${enabled}`);
+			setState((prevState) => ({ ...prevState, bluetoothEnabled: enabled }));
+		} catch (error) {
+			console.log('App::componentDidMount Status Error: ', error);
+			setState((prevState) => ({ ...prevState, bluetoothEnabled: false }));
+		}
+	};
 
-  const onStateChanged = (stateChangedEvent: StateChangeEvent) => {
-    console.log('App::onStateChanged event used for onBluetoothEnabled and onBluetoothDisabled');
+	const onStateChanged = (stateChangedEvent: StateChangeEvent) => {
+		console.log('App::onStateChanged event used for onBluetoothEnabled and onBluetoothDisabled');
 
-    setState((prevState) => ({
-      ...prevState,
-      bluetoothEnabled: stateChangedEvent.enabled,
-      device: stateChangedEvent.enabled ? prevState.device : undefined,
-    }));
-  };
+		setState((prevState) => ({
+			...prevState,
+			bluetoothEnabled: stateChangedEvent.enabled,
+			device: stateChangedEvent.enabled ? prevState.device : undefined,
+		}));
+	};
 
-  useEffect(() => {
-    console.log('App::componentDidMount adding listeners: onBluetoothEnabled and onBluetoothDistabled');
-    console.log('App::componentDidMount alternatively could use onStateChanged');
-    enabledSubscription = RNBluetoothClassic.onBluetoothEnabled(onStateChanged);
-    disabledSubscription = RNBluetoothClassic.onBluetoothDisabled(onStateChanged);
+	useEffect(() => {
+		console.log('App::componentDidMount adding listeners: onBluetoothEnabled and onBluetoothDistabled');
+		console.log('App::componentDidMount alternatively could use onStateChanged');
+		enabledSubscription = RNBluetoothClassic.onBluetoothEnabled(onStateChanged);
+		disabledSubscription = RNBluetoothClassic.onBluetoothDisabled(onStateChanged);
 
-    checkBluetoothEnabled();
+		checkBluetoothEnabled();
 
-    return () => {
-      console.log('App:componentWillUnmount removing subscriptions: enabled and disabled');
-      console.log('App:componentWillUnmount alternatively could have used stateChanged');
-      enabledSubscription.remove();
-      disabledSubscription.remove();
-    };
-  }, []);
+		return () => {
+			console.log('App:componentWillUnmount removing subscriptions: enabled and disabled');
+			console.log('App:componentWillUnmount alternatively could have used stateChanged');
+			enabledSubscription.remove();
+			disabledSubscription.remove();
+		};
+	}, []);
 
 	return (
-		<Wrapper      
-		from={{
-			translateY: -128,
-			opacity: 0.5,
-		}}
-		animate={{
-			translateY: 0,
-			opacity: 1,
-		}}
-	>
+		<Wrapper
+			from={{
+				translateY: -128,
+				opacity: 0.5,
+			}}
+			animate={{
+				translateY: 0,
+				opacity: 1,
+			}}
+		>
 			<Header />
 
+
 			<Spacer h={8} />
-	
+
 			<Container
 				contentContainerStyle={{ flexGrow: 1 }}
 				showsVerticalScrollIndicator={false}
 				scrollEnabled={scrollEnabled}>
-				<ContainerImg 
-				     from={{
-							translateY: 300,
-							opacity: 0.5,
-						}}
-						animate={{
-							translateY: 0,
-							opacity: 1,
-						}}
-					transition={{type:'spring'}}
+				<ContainerImg
+					from={{
+						translateY: 300,
+						opacity: 0.5,
+					}}
+					animate={{
+						translateY: 0,
+						opacity: 1,
+					}}
+					transition={{ type: 'spring' }}
 				>
 					<Image
 						style={{
@@ -223,131 +222,62 @@ export function Device() {
 
 				<Spacer h={16} />
 
-			{device?.isBluetooth && (
-					<>	
-				 {!state.device ? (
-    		    <DeviceListScreen
-    		      selectDevice={selectDevice}
-    		    />
-    		  ) : (
-				<>
-							{state.device !== null&&(
-	   					<ElementConnectedDevice 
-		 					connectToDevice={selectDevice}
-		 					connectedDevice={state.device ? state.device: null}
-								/>
-					)}
-				<ContainerEqualizer>
-					<HeaderEqualizer 
-					onOpen={openModal}
-					handlePreset={handlePreset}
-					handleScrollEnabled={handleScrollEnabled}
-					handleModalEdit={handleModalEdit}
-					/>
-        <ConnectionScreen
-          device={state.device}
-					handleScrollEnabled={handleScrollEnabled}
-          onBack={() => setState((prevState) => ({ ...prevState, device: undefined }))}
-        />
-				</ContainerEqualizer>
+				{device?.isBluetooth && (
+					<>
+						{!state.device ? (
+							<DeviceListScreen
+								selectDevice={selectDevice}
+							/>
+						) : (
+							<>
+								{state.device !== null && (
+									<ElementConnectedDevice
+										connectToDevice={selectDevice}
+										connectedDevice={state.device ? state.device : null}
+									/>
+								)}
+								<ContainerEqualizer>
+									<HeaderEqualizer
+										onOpen={openModal}
+										handlePreset={handlePreset}
+										handleScrollEnabled={handleScrollEnabled}
+										handleModalEdit={handleModalEdit}
+									/>
+									<ConnectionScreen
+										device={state.device}
+										handleScrollEnabled={handleScrollEnabled}
+										onBack={() => setState((prevState) => ({ ...prevState, device: undefined }))}
+									/>
+								</ContainerEqualizer>
 
-				<ContainerCarousel>
-					
-					
-		
-	
+								<ContainerCarousel>
 
-					<CarouselProfile
-						titleProfile={'Perfis Personalidades'}
-						data={personalities}
-					/>
 
-					<Spacer h={16} />
 
-					<CarouselProfile
-						titleProfile={'Perfis Públicos '}
-						data={profiles}
-						isPersonalities={false}
-					/>
-				</ContainerCarousel>
-				<Spacer h={32} />
 
-				<View style={{ flex: 1 }} />
 
-				<BoxButtons
-					style={{
-						paddingHorizontal: scale(16)
-					}}>
-					<ButtonSquare
-						label="Suporte"
-						onPress={() =>
-							navigation.navigate('Support', {
-								deviceId: device?.id
-							})
-						}>
-						<Image
-							source={Headset}
-							style={{
-								width: 32,
-								height: 32
-							}}
-							resizeMode="contain"
-						/>
-					</ButtonSquare>
-						
-					<Spacer w={16} />
-						
-					<ButtonSquare
-						label="Tutorias de uso"
-						onPress={() =>
-							navigation.navigate('Tutorials', {
-								deviceId: device?.id
-							})
-						}>
-						<Image
-							source={Info}
-							style={{
-								width: 32,
-								height: 32
-							}}
-							resizeMode="contain"
-						/>
-					</ButtonSquare>
-						
-					{device.isBluetooth && (
-						<>
-							<Spacer w={16} />
-							<ButtonSquare
-								label="Configurações"
-								onPress={() =>
-									navigation.navigate('SettingsEarphone')
-								}>
-								<Image
-									source={Settings}
-									style={{
-										width: 32,
-										height: 32
-									}}
-									resizeMode="contain"
-								/>
-							</ButtonSquare>
-						</>
-					)}
-				</BoxButtons>
-				</>
-      )}  
-					
+									<Spacer h={16} />
+
+									<CarouselProfile
+										titleProfile={'Perfis Públicos '}
+										data={profiles}
+										isPersonalities={false}
+									/>
+								</ContainerCarousel>
+								<Spacer h={32} />
+
+								<View style={{ flex: 1 }} />
+
+
+							</>
+						)}
+
 					</>
 				)}
 
-		
 
-				<Footer>
-					<Button
-						title="Voltar"
-						onPress={() => navigation.goBack()}
-					/>
-				</Footer>
+
+
 			</Container>
 
 			<ModalPreset
