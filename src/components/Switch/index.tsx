2d1dfc60660cddb75845bf5872e7bf7d8144a9b3
wesleywcr <wesley.rodrigues@clicksoft.com.br>
2024-12-12 17:47:29 -0300

    feat: add component switch

---
+++ src/components/Switch/index.tsx
@@ -0,0 +1,89 @@
+import {
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  SharedValue,
+} from "react-native-reanimated";
+import * as S from "./styles";
+
+
+interface ISwitchProps {
+  value: SharedValue<number>;
+  onPress: () => void;
+  duration?: number;
+  trackColors?: {
+    on: string;
+    off: string;
+  };
+}
+const Switch = ({
+  value,
+  onPress,
+  duration = 400,
+  trackColors = {
+    on: '#ffff',
+    off: "#fff",
+  },
+}: ISwitchProps) => {
+  const height = useSharedValue(0);
+  const width = useSharedValue(0);
+
+  const trackAnimatedStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      value.value,
+      [0, 1],
+      [trackColors.off, trackColors.on],
+    );
+    const colorBackground = interpolateColor(
+      value.value,
+      [0, 1],
+      ["#777777","#6edc5f"],
+    );
+    const colorValue = withTiming(color, {duration});
+
+    return {
+      backgroundColor: colorBackground,
+      borderWidth: 1,
+      borderColor: colorValue,
+      borderRadius: height.value / 2,
+    };
+  });
+
+  const thumbAnimatedStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      value.value,
+      [0, 1],
+      [trackColors.off, trackColors.on],
+    );
+    const colorValue = withTiming(color, {duration});
+
+    const moveValue = interpolate(
+      Number(value.value),
+      [0, 1],
+      [0, width.value - height.value],
+    );
+    const translateValue = withTiming(moveValue, {duration});
+
+    return {
+      transform: [{translateX: translateValue}],
+      borderRadius: height.value / 2,
+      backgroundColor: colorValue,
+    };
+  });
+
+  return (
+    <S.WrapperSwitch onPress={onPress}>
+      <S.Track
+        onLayout={e => {
+          height.value = e.nativeEvent.layout.height;
+          width.value = e.nativeEvent.layout.width;
+        }}
+        style={trackAnimatedStyle}>
+        <S.Thumb style={thumbAnimatedStyle}></S.Thumb>
+      </S.Track>
+    </S.WrapperSwitch>
+  );
+};
+export default Switch;
---
+++ src/components/Switch/styles.ts
@@ -0,0 +1,20 @@
+
+import Animated from "react-native-reanimated";
+import {scale} from "react-native-size-matters";
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+
+export const WrapperSwitch = styled.Pressable``;
+
+export const Track = styled(Animated.View)`
+  align-items: "flex-start";
+  width: ${scale(52)}px;
+  height: ${scale(28)}px;
+  padding: ${scale(5)}px;
+`;
+export const Thumb = styled(Animated.View)`
+  height: 100%;
+  aspect-ratio: 1;
+  background-color: ${({theme})=> theme.COLORS.white_100};
+`;
