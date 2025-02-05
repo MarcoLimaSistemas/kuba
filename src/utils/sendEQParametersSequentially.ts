4d528333776293588a49f0d4c7103d764147295e
wesleywcr <wesley.rodrigues@clicksoft.com.br>
2025-02-05 14:22:06 -0300

    feat: add function sendEQParametersSequentially

---
+++ src/utils/sendEQParametersSequentially.ts
@@ -0,0 +1,25 @@
+import { IBandSettings } from "@models/band";
+import { DeviceEventEmitter } from "react-native";
+
+  export const sendEQParametersSequentially = (newArray: IBandSettings[]) => {
+    newArray.forEach((band) => {
+
+      const bandId = band.id;
+
+      const typeFrequency = "frequency"
+      const typeGain = "gain"
+      const typeQuality = "quality"
+
+      const frequency = band.frequency
+      const gain = band.gain
+      const quality = band.quality
+
+
+
+      DeviceEventEmitter.emit("onEventEqualizer", { bandId, type: typeFrequency, value: frequency })
+      DeviceEventEmitter.emit("onEventEqualizer", { bandId, type: typeGain, value: gain })
+      DeviceEventEmitter.emit("onEventEqualizer", { bandId, type: typeQuality, value: quality })
+
+
+    });
+  }
